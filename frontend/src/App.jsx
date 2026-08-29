c83cdcb4d56883b355e6c2160b2017875307c11e
Praveen Joshi <praveenjoshi96545-source@users.noreply.github.com>
2026-08-29 20:04:14 +0530

    Connect frontend to Render backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
     try {
 
       const response = await fetch(
-        "http://127.0.0.1:5000/upload",
+        "https://ai-resume-analyzer-2-qiyu.onrender.com/upload",
         {
           method: "POST",
           body: formData
